3206f42207d71a28b7878dc66249612916c33a17
Romain Durand <sirius2006@gmail.com>
2016-05-30 00:22:32 +0200

    full offline experience

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,12 @@
 this.addEventListener("fetch", event => {
+	const url = new URL(event.request.url);
+	if (url.origin === location.origin && url.pathname === "/") {
+		event.respondWith(caches.match("index.html"));
+		return;
+	}
 	event.respondWith(
 		caches.match(event.request)
 		.then(response => response || fetch(event.request))
-		.catch(() => {
-			if(event.request.mode === "navigate") {
-				return caches.match("index-offline.html");
-			}
-		})
 	);
 });
 
@@ -16,8 +16,13 @@ this.addEventListener("install", (event) => {
 	event.waitUntil(
 		caches.open("v1").then(cache => 
 			cache.addAll([
-				"index-offline.html",
-				"style.css"
+				"index.html",
+				"style.css",
+				"images.json",
+				"images/internet.jpg",
+				"images/wat.jpg",
+				"images/inception.jpg",
+				"script.js"
 			])
 		)
 	);
