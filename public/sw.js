4fbcedb8dd3776b7807cc5def3af381059f8b4b9
Romain Durand <sirius2006@gmail.com>
2016-05-29 23:45:37 +0200

    added a simple offline experience

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,24 @@
 this.addEventListener("fetch", event => {
-	const url = new URL(event.request.url);
-	if (url.pathname.endsWith(".jpg")) {
-		event.respondWith(
-			fetch("images/haha.jpg")
-		);
-	}
+	event.respondWith(
+		caches.match(event.request)
+		.then(response => response || fetch(event.request))
+		.catch(() => {
+			if(event.request.mode === "navigate") {
+				return caches.match("index-offline.html");
+			}
+		})
+	);
+});
+
+this.addEventListener("install", (event) => {
+	console.log("installing....");
+
+	event.waitUntil(
+		caches.open("v1").then(cache => 
+			cache.addAll([
+				"index-offline.html",
+				"style.css"
+			])
+		)
+	);
 });
